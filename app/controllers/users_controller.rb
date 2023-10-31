07f7c0e5e9df5eadb889980b642a6e747951adfd
Zihao C <62597814+czh2152215@users.noreply.github.com>
2023-10-30 22:13:25 -0400

    delete files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(session[:user_id])
     if @user.update(user_params)
-      redirect_to user_path(@user), notice: '用户信息已更新'
+      redirect_to user_path(@user), notice: 'User Info Updated'
     else
       render :edit
     end
