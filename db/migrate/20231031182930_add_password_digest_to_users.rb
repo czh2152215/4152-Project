5ae21500bb7a2ed0b1ea9499e1fe21b2e8af56fb
shulinc1 <shulinc2@uci.edu>
2023-11-01 20:14:14 -0400

    Update 20231031182930_add_password_digest_to_users.rb

--- db/migrate/20231031182930_add_password_digest_to_users.rb
+++ db/migrate/20231031182930_add_password_digest_to_users.rb
@@ -1,7 +1,8 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[6.0]
   def change
-    unless column_exists? :users, :password_digest
-      add_column :users, :password_digest, :string
-    end
+    # unless column_exists? :users, :password_digest
+
+    # end
+    add_column :users, :password_digest, :string
   end
 end
