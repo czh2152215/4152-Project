126861482cebb701667911f5a49cfb69a378a54f
Yixiao Yuan <eshoyuan@gmail.com>
2023-12-02 17:20:44 -0500

    add audio

--- db/schema.rb
+++ db/schema.rb
@@ -67,8 +67,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_210810) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "favorites", "artworks"
   add_foreign_key "favorites", "users"
 end
