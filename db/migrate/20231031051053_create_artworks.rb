d2a2e064ce52a1268f14872d29494b5dd2337934
Yixiao Yuan <eshoyuan@gmail.com>
2023-12-04 15:41:40 -0500

    fix ID bug in seeds.rb

--- db/migrate/20231031051053_create_artworks.rb
+++ db/migrate/20231031051053_create_artworks.rb
@@ -1,7 +1,6 @@
 class CreateArtworks < ActiveRecord::Migration[7.1]
   def change
     create_table :artworks do |t|
-      t.string :id
       t.string :artwork_name
       t.text :background_information
       t.string :author
